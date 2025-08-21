de1df9e0e513bfea24b9d71de328ffee7d15530f
ajg-iz <andreas.goebel@gmail.com>
2025-08-21 12:33:33 +0200

    aufgabe 10

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,39 +10,102 @@ public class Main {
         String favourite_saying;
         Double number1;
         Double number2;
+        Boolean isNumber;
         String operator;
+        char[] validOperators;
+        Boolean isValidOperator;
         Scanner scanner = new Scanner(System.in);
 
         try {
-            System.out.print("Name: ");
-            name = scanner.nextLine();
+            name="";
+            while (name.trim().isEmpty()) {
+                System.out.print("Name: ");
+                name = scanner.nextLine();
+                if (name.trim().isEmpty()) {
+                    System.out.println("Invalid name.");
+                }
+            }
 
-            System.out.print("Age: ");
-            age = scanner.nextInt();
-            scanner.nextLine();
+            age=0;
+            isNumber=false;
+            while (!isNumber) {
+                System.out.print("Your age: ");
+                try {
+                    age = scanner.nextInt();
+                    isNumber=true;
+                } catch(Exception e) {
+                    System.out.println("Invalid number.");
+                }
+                scanner.nextLine();
+            }
+
+
+            favourite_saying = "";
+            while (favourite_saying.trim().isEmpty()) {
+                System.out.print("Your favourite saying: ");
+                favourite_saying = scanner.nextLine();
+                if(favourite_saying.trim().isEmpty()){
+                    System.out.println("Invalid favourite saying.");
+                }
+            }
+
+            number1=0.0;
+            isNumber=false;
+            while (!isNumber) {
+                System.out.print("First number: ");
+                try {
+                    number1 = scanner.nextDouble();
+                    isNumber=true;
+                } catch(Exception e) {
+                    System.out.println("Invalid number.");
+                }
+                scanner.nextLine();
+            }
+
+
+            number2=0.0;
+            isNumber=false;
+            while (!isNumber) {
+                System.out.print("Second number: ");
+                try {
+                    number2 = scanner.nextDouble();
+                    isNumber=true;
+                } catch(Exception e) {
+                    System.out.println("Invalid number.");
+                }
+                scanner.nextLine();
+            }
+
+            operator = "";
+            validOperators = new char[5];
+            validOperators[0] = '+';
+            validOperators[1] = '-';
+            validOperators[2] = '*';
+            validOperators[3] = '/';
+            validOperators[4] = '%';
+            isValidOperator = false;
+
+            while (operator.trim().isEmpty() || !isValidOperator) {
+                System.out.print("Operator:");
+                operator = scanner.nextLine();
+                if (!operator.trim().isEmpty()) {
+                    for (int i = 0; i < validOperators.length; i++) {
+                        if (operator.trim().charAt(0) == validOperators[i]) {
+                            isValidOperator = true;
+                            break;
+                        }
+                    }
+                    if (!isValidOperator) {
+                        System.out.println("Invalid operator.");
+                    }
+                }
+            }
             
 
-            System.out.print("Your favourite saying: ");
-            favourite_saying = scanner.nextLine();
-            
-
-            System.out.print("First number: ");
-            number1 = scanner.nextDouble();
-            scanner.nextLine();
-            
-
-            System.out.print("Second number: ");
-            number2 = scanner.nextDouble();
-            scanner.nextLine();
-            
-
-            System.out.print("Operator:");
-            operator = scanner.nextLine();
-            
-
-            System.out.println("Hello " + name + ". You are "+age+" years old.\n Your favourite saying is \"" + favourite_saying + "\".");
-            System.out.println("You asked to solve " + number1 +""+ operator +""+ number2 + ".");
+            System.out.println("Hello " + name + ". You are "+age+" years old.\nYour favourite saying is \"" + favourite_saying + "\".");
+            System.out.println("You asked to solve " + number1 + operator + number2 + ".");
             System.out.println("The result of that operation is: " + performOperation(number1, number2, operator));
+
         } catch (Exception e) {
             System.out.printf(e.getMessage());
         }
@@ -60,6 +124,11 @@ public class Main {
                     throw new IllegalArgumentException("Cannot divide by zero.");
                 }
                 return number1 / number2;
+            case "%":
+                if (number2 == 0) {
+                    throw new IllegalArgumentException("Cannot divide by zero.");
+                }
+                return number1 % number2;
             default:
                 throw new IllegalArgumentException("Invalid operator: " + operator);
         }
